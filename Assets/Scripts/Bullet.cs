803bc762c39d6e1ac0d4aa7be9ce8c2760e6bcf9
epamatz <40709011+epamatz@users.noreply.github.com>
2021-03-08 00:26:49 -0800

    Add files via upload

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Bullet : MonoBehaviour
+{
+    private Transform bullet;
+  public float Speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+      bullet = GetComponent< Transform> ();
+    }
+
+    void FixedUpdate(){
+      bullet.position += Vector3.up *Speed;
+
+      if(bullet.position.y >= 10)
+        Destroy (gameObject);
+    }
+  
+    void OnTriggerEnter2D(Collider2D other){
+      if(other.tag == "Enemy"){
+        Destroy (other.gameObject);
+        Destroy (gameObject);
+        PlayerScore.playerScore+=10;
+      }
+      else if (other.tag =="Base")
+       Destroy (gameObject);
+    }
+}
---
+++ Assets/Scripts/EnemyBullet.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour
+{
+    private Transform bullet;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bullet = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        bullet.position += Vector3.up* -speed;
+        if(bullet.position.y <= -10)
+            Destroy (bullet.gameObject);
+    }
+    void OnTriggerEnter2D(Collider2D other){
+        if(other.tag == "Player"){
+            Destroy (other.gameObject);
+            Destroy (gameObject);
+        }
+        else if (other.tag =="Base"){
+            GameObject playerBase = other.gameObject;
+            Basehealth basehealth = playerBase.GetComponent<Basehealth> ();
+            basehealth.health -= 1;
+            Destroy (gameObject);
+        }
+    }
+}
